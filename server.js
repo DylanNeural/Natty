77063b80b3650b34d9190618851a045e4c283b63
Dylan Andrade Pereira <dylan-psupp@outlook.fr>
2026-02-18 15:06:10 +0100

    Allow CORS from localhost:5001
    Add http://localhost:5001 to the CORS origin list in server.js so requests from that development port are permitted. No other CORS settings (credentials, methods, headers) were changed.

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(helmet());
 // CORS avec options correctes
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:5001", "http://localhost:3000", "http://localhost:3001"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
